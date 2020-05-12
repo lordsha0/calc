54e0692f7e73c96d9e6ded41154832c682b39c33
lordsha0 <32988899+lordsha0@users.noreply.github.com>
2020-05-12 19:17:53 -0400

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import sys
+
+try:
+    print(sys.argv[1])
+    if sys.argv[1] == 'a':
+        print(float(sys.argv[2]) + float(sys.argv[3]))
+    if sys.argv[1] == 'd':
+        print(float(sys.argv[2]) / float(sys.argv[3]))
+    if sys.argv[1] == 'm':
+        print(float(sys.argv[2]) * float(sys.argv[3]))
+    if sys.argv[1] == 's':
+        print(float(sys.argv[2]) - float(sys.argv[3]))
+except:
+    print('You need to specify numbers to operate with.')
